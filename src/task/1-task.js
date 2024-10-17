0ce528afc9cded9a10f82356c57ca700c26865a2
East <eastmaels@gmail.com>
2024-10-18 01:43:12 +0900

    update readme

--- src/task/1-task.js
+++ src/task/1-task.js
@@ -1,14 +1,25 @@
 import { namespaceWrapper } from "@_koii/namespace-wrapper";
+import { gitTask } from './GitTask.js';
+import { searchRandomRepo } from './github.js';
+
 
 export async function task(roundNumber) {
   // Run your task and store the proofs to be submitted for auditing
   // The submission of the proofs is done in the submission function
   try {
-    console.log(`EXECUTE TASK FOR ROUND ${roundNumber}`);
-    console.log("Started Task", new Date(), "TEST");
-    // you can optionally return this value to be used in debugging
-    await namespaceWrapper.storeSet("value", "Hello, World!");
-  } catch (error) {
-    console.error("EXECUTE TASK ERROR:", error);
+    console.log('task called with round', roundNumber);
+    const randomRepo = await searchRandomRepo();
+    console.log('randomRepo.clone_url', randomRepo.clone_url)
+    gitTask.setRepo(randomRepo);
+    await gitTask.clone();
+    await gitTask.zipRepo();
+    const cid = await gitTask.storeFile();
+    await gitTask.cleanup();
+
+    await namespaceWrapper.storeSet('cid', cid);
+    return 'Done';
+  } catch (err) {
+    console.error('ERROR IN EXECUTING TASK', err);
+    return 'ERROR IN EXECUTING TASK' + err;
   }
 }
