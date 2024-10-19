1a70fd25497fb68722e9ac91ffb56d98e6eac798
Laura Abro <laura@koii.network>
2024-10-19 07:57:09 -0300

    switch to CJS

--- src/task/github.cjs
+++ src/task/github.cjs
@@ -1,26 +1,23 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const GITHUB_API_URL = 'https://api.github.com';
+const GITHUB_API_URL = "https://api.github.com";
 const ACCESS_TOKEN = process.env.GIT_ACCESS_TOKEN; // Replace with your actual token
 const TEST_KEYWORD = process.env.TEST_KEYWORD; // You can change this to any keyword you like
 const MAX_REPO_SIZE_BYTES = 10 * 1024 * 1024; // Example condition: 100 MB
 
-export async function searchRandomRepo() {
+async function searchRandomRepo() {
   try {
-    const response = await axios.get(
-      `${GITHUB_API_URL}/search/repositories`,
-      {
-        headers: {
-          Authorization: `token ${ACCESS_TOKEN}`,
-          Accept: 'application/vnd.github.v3+json',
-        },
-        params: {
-          q: TEST_KEYWORD,
-          sort: 'stars',
-          order: 'desc'
-        }
+    const response = await axios.get(`${GITHUB_API_URL}/search/repositories`, {
+      headers: {
+        Authorization: `token ${ACCESS_TOKEN}`,
+        Accept: "application/vnd.github.v3+json",
       },
-    );
+      params: {
+        q: TEST_KEYWORD,
+        sort: "stars",
+        order: "desc",
+      },
+    });
 
     if (response.status != 200) {
       throw new Error(`Error fetching data: ${response.statusText}`);
@@ -35,12 +32,14 @@ export async function searchRandomRepo() {
       while (!foundRepo) {
         const randomIndex = Math.floor(Math.random() * repositories.length);
         const randomRepo = repositories[randomIndex];
-         // Remove the selected repository from the list
+        // Remove the selected repository from the list
         repositories.splice(randomIndex, 1);
 
         // Size is returned in KB, convert to bytes
         const repoSizeBytes = randomRepo.size * 1024;
-        console.log(`Repository: ${randomRepo.full_name}, Size: ${repoSizeBytes} bytes`);
+        console.log(
+          `Repository: ${randomRepo.full_name}, Size: ${repoSizeBytes} bytes`,
+        );
 
         if (repoSizeBytes <= MAX_REPO_SIZE_BYTES) {
           return randomRepo;
@@ -49,11 +48,12 @@ export async function searchRandomRepo() {
           continue;
         }
       }
-
     } else {
-      console.log('No repositories found.');
+      console.log("No repositories found.");
     }
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 }
+
+module.exports = { searchRandomRepo };
